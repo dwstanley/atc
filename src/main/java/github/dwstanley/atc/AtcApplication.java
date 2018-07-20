01ce72162a8514900229196a3bbb7939b0d9a2cb
dwstanley <skndarren@gmail.com>
2018-07-20 16:31:02 -0400

    Initial project structure

---
+++ src/main/java/github/dwstanley/atc/AtcApplication.java
@@ -0,0 +1,12 @@
+package github.dwstanley.atc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtcApplication.class, args);
+	}
+}
---
+++ src/test/java/github/dwstanley/atc/AtcApplicationTests.java
@@ -0,0 +1,16 @@
+package github.dwstanley.atc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AtcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
